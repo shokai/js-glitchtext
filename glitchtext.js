b5fea2f5719cbd74ddd0db00136cd7cf4ff2037f
Sho Hashimoto <hashimoto@shokai.org>
2011-02-06 21:08:25 +0900

    glitch_doubleChar

--- glitchtext.js
+++ glitchtext.js
@@ -31,6 +31,12 @@ var GlitchText = function(){
     };
 
     this.glitch_doubleChar = function(str){
-        
+        var res = '';
+        ss = str.split('');
+        for(i in ss){
+            var c = ss[i];
+            if(!(c.match(/[ 　\t]/))) res += c+c
+        };
+        return res;
     };
 };
