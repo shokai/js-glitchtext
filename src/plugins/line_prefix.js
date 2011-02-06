6e241f18808c1864f31a1ded57e37188b1fb3dc3
Sho Hashimoto <hashimoto@shokai.org>
2011-02-07 08:43:42 +0900

    fix lineprefix

--- src/plugins/line_prefix.js
+++ src/plugins/line_prefix.js
@@ -2,7 +2,7 @@ GlitchText.prototype.glitch_linePrefix = function(str){
     var res = '───';
     while(true){
         res += '─';
-        if(Math.random() > 0.8) break;
+        if(Math.random() > 0.5) break;
     };
     return res+str;
 };
