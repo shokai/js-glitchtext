ffc46f9d6dd6fd1d8cceab2304be321c6bfc5f8c
Sho Hashimoto <hashimoto@shokai.org>
2011-02-06 21:03:34 +0900

    first commit

---
+++ glitchtext.js
@@ -0,0 +1,36 @@
+
+var Glitch = function(){
+    this.random = function(str){
+        var ms = this.methods();
+        var m = ms[Math.floor(Math.random()*ms.length)];
+        return this[m](str);
+    };
+
+    this.methods = function(){
+        var ms = new Array();
+        for(m in this){
+            if(m.match(/^glitch_.+/)) ms.push(m);
+        };
+        return ms;
+    };
+
+    this.glitch_reverse = function(str){
+        return str.split('').reverse().join('');
+    };
+
+    this.glitch_addspace = function(str){
+        return str.split('').join(' ');
+    };
+
+    this.glitch_atai = function(str){
+        return "＼"+str+"／";
+    };
+
+    this.glitch_double = function(str){
+        return str + " " + str;
+    };
+
+    this.glitch_doubleChar = function(str){
+        
+    };
+};
---
+++ test.js
@@ -0,0 +1,12 @@
+load('glitchtext.js');
+
+g = new Glitch();
+
+s = "今日はかずすけ楽しかったねーまた遊ぼうねー今度はお弁当持って行こう"
+print("source : "+s);
+
+var ms = g.methods();
+for(i in ms){
+    m = ms[i];
+    print(m+" : "+g[m](s));
+}
