073165ae128acb88ef2158e03d7bf086a6307199
Sho Hashimoto <hashimoto@shokai.org>
2011-02-07 08:32:54 +0900

    plugin face

--- src/plugins/face.js
+++ src/plugins/face.js
@@ -1,38 +1,3 @@
-var GlitchText = function(){
-    this.random = function(str){
-        var ms = this.methods();
-        var m = ms[Math.floor(Math.random()*ms.length)];
-        return this[m](str);
-    };
-
-    this.methods = function(){
-        var ms = new Array();
-        for(m in this){
-            if(m.match(/^glitch_.+/)) ms.push(m);
-        };
-        return ms;
-    };
-
-};
-
-GlitchText.prototype.glitch_addspace = function(str){
-    return str.split('').join(' ');
-};
-GlitchText.prototype.glitch_atai = function(str){
-    return "＼"+str+"／";
-};
-GlitchText.prototype.glitch_double = function(str){
-    return str + " " + str;
-};
-GlitchText.prototype.glitch_doubleChar = function(str){
-    var res = '';
-    ss = str.split('');
-    for(i in ss){
-        var c = ss[i];
-        if(!(c.match(/[ 　\t]/))) res += c+c
-    };
-    return res;
-};
 GlitchText.prototype.glitch_face = function(str){
     var faces = ['(○´･д･)ﾉ〔● ｫ\'`ｮｩ○〕ヽ(･д･`●) ',
 'p≪ﾟ+｡ぉﾚ£∋ぅ｡+ﾟ≫q(´I｀●) ',
@@ -7115,24 +7080,4 @@ GlitchText.prototype.glitch_face = function(str){
 '新体操リボンダゼ☆(＾＾)ξ゛ｸﾙｸﾙ',
 '（*´∀｀）つ＝＝＝＝●'];
     return str + ' ' + faces[Math.floor(Math.random()*faces.length)];
-};
-GlitchText.prototype.glitch_ignoreSearch = function(str){
-    return str.split('').join('/');
-};
-GlitchText.prototype.glitch_insertWave = function(str){
-    var res = '';
-    var ss = str.split('');
-    for(i in ss){
-        res += ss[i];
-        if(Math.random() > 0.8) res += '〜';
-    };
-    return res;
-};
-GlitchText.prototype.glitch_kaibu = function(str){
-    var rev = str.split('').reverse();
-    rev.shift();
-    return str + rev.join('');
-};
-GlitchText.prototype.glitch_reverse = function(str){
-    return str.split('').reverse().join('');
 };
