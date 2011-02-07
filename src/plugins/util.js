4c20ec927f201fe8050fba9bf8625b7407501eef
Sho Hashimoto <hashimoto@shokai.org>
2011-02-07 20:04:03 +0900

    remove Array.each

--- src/plugins/util.js
+++ src/plugins/util.js
@@ -2,14 +2,8 @@ Array.prototype.choice = function(){
     return this[Math.floor(Math.random()*this.length)];
 };
 
-Array.prototype.each = function(func){
-    for(var i = 0; i < this.length; i++){
-        func(this[i]);
-    };
-};
-
 Array.prototype.map = function(func){
     var res = new Array();
-    this.each(function(i){res.push(func(i))});
+    this.forEach(function(i){res.push(func(i))});
     return res;
 };
--- test/test.js
+++ test/test.js
@@ -3,10 +3,9 @@ g = new GlitchText();
 ss = ['今日はかずすけ楽しかったねー マタ遊ボウネー 今度はお弁当持って行こう',
       'ここにソース置いた https://github.com/shokai/js-glitchtext']
 
-ss.each(function(s){
+ss.forEach(function(s){
     print("source : "+s);
-    
-    g.methods().each(function(m){
+    g.methods().forEach(function(m){
         print(m+" : "+g[m](s));
     });
 });
