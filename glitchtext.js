de4690de14782ba56f2429feadb2b25b87f78f92
Sho Hashimoto <hashimoto@shokai.org>
2011-02-07 18:52:39 +0900

    load plugin cmabridge when tiny_segmenter already loaded

--- glitchtext.js
+++ glitchtext.js
@@ -26,17 +26,23 @@ GlitchText.prototype.glitch_atai = function(str){
     return "＼"+str+"／";
 };
 
-GlitchText.prototype.glitch_cmabridge = function(str){
-    var tseg = new TinySegmenter();
-    return tseg.segment(str).map(function(seg){
-        seg = seg.split('');
-        var i = Math.floor(Math.random()*(seg.length-1));
-        var tmp = seg[i];
-        seg[i] = seg[i+1];
-        seg[i+1] = tmp;
-        return seg.join('');
-    }).join('');
-};
+try{
+    if(typeof TinySegmenter == 'function'){
+        GlitchText.prototype.glitch_cmabridge = function(str){
+            var tseg = new TinySegmenter();
+            return tseg.segment(str).map(function(seg){
+                seg = seg.split('');
+                var i = Math.floor(Math.random()*(seg.length-1));
+                var tmp = seg[i];
+                seg[i] = seg[i+1];
+                seg[i+1] = tmp;
+                return seg.join('');
+            }).join('');
+        };
+    }
+}
+catch(e){
+}
 
 GlitchText.prototype.glitch_double = function(str){
     return str + " " + str;
