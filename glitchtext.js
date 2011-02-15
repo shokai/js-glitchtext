9683ea695ead9a3d31370df8c7dc0d9e596ef368
Sho Hashimoto <hashimoto@shokai.org>
2011-02-15 20:43:06 +0900

    plugin shoooooookai

--- glitchtext.js
+++ glitchtext.js
@@ -371,6 +371,17 @@ GlitchText.prototype.glitch_sekine = function(str){
     return str.join('');
 };
 
+GlitchText.prototype.glitch_shoooooookai = function(str){
+    var arr = str.split('');
+    for(var i = 0; i < arr.length-1; i++){
+        if(arr[i] == arr[i+1]){
+            var count = Math.random()*5;
+            for(var j = 0; j < count; j++) arr[i] += arr[i];
+        };
+    };
+    return arr.join('');
+};
+
 GlitchText.prototype.glitch_speak = function(str){
     var speak = [
         ['「','」'],
---
+++ src/plugins/shoooooookai.js
@@ -0,0 +1,10 @@
+GlitchText.prototype.glitch_shoooooookai = function(str){
+    var arr = str.split('');
+    for(var i = 0; i < arr.length-1; i++){
+        if(arr[i] == arr[i+1]){
+            var count = Math.random()*5;
+            for(var j = 0; j < count; j++) arr[i] += arr[i];
+        };
+    };
+    return arr.join('');
+};
