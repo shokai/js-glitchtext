7ef8a9a9695f0a1accd134d454f35614ce131b0c
Sho Hashimoto <hashimoto@shokai.org>
2011-03-04 03:48:10 +0900

    fix plugin crack

--- glitchtext.js
+++ glitchtext.js
@@ -52,12 +52,12 @@ catch(e){
 }
 
 GlitchText.prototype.glitch_crack = function(str){
-    return str + [
-        '";CREATE TABLE "glitchtweet.com"',
-        '"><script>window.alert("peropero")</script>',
-        '"><marquee>',
-        '"><script>location.href="http://glitchtweet.com"</script>',
-        '"><link rel="stylesheet" href="http://ma.la/tmp/youpy.css">'
+    return [
+        str+'";CREATE TABLE "glitchtweet.com"',
+        '"><script>alert("'+str+'")</script>',
+        str+'"><marquee>',
+        str+'"><script>location.href="http://glitchtweet.com"</script>',
+        str+'"><link rel="stylesheet" href="http://ma.la/tmp/youpy.css">'
     ].choice();
 };
 GlitchText.prototype.glitch_double = function(str){
--- src/plugins/crack.js
+++ src/plugins/crack.js
@@ -1,9 +1,9 @@
 GlitchText.prototype.glitch_crack = function(str){
-    return str + [
-        '";CREATE TABLE "glitchtweet.com"',
-        '"><script>window.alert("peropero")</script>',
-        '"><marquee>',
-        '"><script>location.href="http://glitchtweet.com"</script>',
-        '"><link rel="stylesheet" href="http://ma.la/tmp/youpy.css">'
+    return [
+        str+'";CREATE TABLE "glitchtweet.com"',
+        '"><script>alert("'+str+'")</script>',
+        str+'"><marquee>',
+        str+'"><script>location.href="http://glitchtweet.com"</script>',
+        str+'"><link rel="stylesheet" href="http://ma.la/tmp/youpy.css">'
     ].choice();
 };
